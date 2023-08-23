14c7af2afd1d5b8eed3c9e3f245838877074077a
fauruz <le.phong1456@gmail.com>
2023-08-23 20:11:29 +0700

    update1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToaDo td = new ToaDo(1,2,"M");
+            Console.WriteLine("{0}", td);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ ToaDo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class ToaDo
+    {
+        private int x;
+        private int y;
+        private string name;
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public ToaDo()
+        {
+            x = 0;
+            y = 0;
+            name = string.Empty;
+        }
+        public ToaDo(int tx,int ty,string n)
+        {
+            x = tx;
+            y = ty;
+            name = n;
+        }
+        public override string ToString()
+        {
+            return name + "(" + x + "," + y + ")";
+        }
+    }   
+
+}
